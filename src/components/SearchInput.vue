2c206a6b11868d9a11e54519c034dbc6ec26e21a
PiterSt <piotr.stanny@gmail.com>
2018-07-20 09:02:46 +0200

    Added simple hero image view

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="searchWrapper">
+        <input
+                id="search"
+                name="search"
+                v-model="searchValue"
+                @input="handleInput"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchInput"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .searchWrapper {
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+    }
+    input {
+        height: 30px;
+        border: 0;
+        border-bottom: 1px solid black;
+        background: none;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input
-        id="search"
-        name="search"
-        v-model="searchValue"
-        @input="handleInput"
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{ item.data[0].description }}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
   import axios from 'axios';
   import debounce from 'lodash.debounce';
+  import Claim from '../components/Claim';
+  import SearchInput from '../components/SearchInput';
 
   const API = 'https://images-api.nasa.gov/search';
 
   export default {
     name: "Search",
+    components: {
+        Claim,
+        SearchInput,
+    },
     data () {
       return {
         searchValue: '',
@@ -48,24 +42,17 @@
 
 <style lang="scss" scoped>
   .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     margin: 0;
     padding: 30px;
     width: 100%;
-  }
-  .search {
+    height: 100vh;
     display: flex;
     flex-direction: column;
-    width: 300px;
-  }
-  label {
-    font-family: Roboto, sans-serif;
-  }
-  input {
-    height: 30px;
-    border: 0;
-    border-bottom: 1px solid black;
+    align-items: center;
+    justify-content: center;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
   }
 </style>
